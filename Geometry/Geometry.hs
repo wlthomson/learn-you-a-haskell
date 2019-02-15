fc1bd66348f8aa18b3cbaff9e35f8ec9ca74392b
wlthomson <will.tho@outlook.com>
2019-02-15 22:12:02 +1300

    Add Cube.hs, Cuboid.hs, Sphere.sh

--- Geometry/Geometry.hs
+++ Geometry/Geometry.hs
@@ -1,29 +1,13 @@
-module Geometry
-( sphereVolume
-, sphereArea
-, cubeVolume
-, cubeArea
-, cuboidArea
-, cuboidVolume
-) where
+import qualified Geometry.Sphere as Sphere
+import qualified Geometry.Cuboid as Cuboid
+import qualified Geometry.Cube as Cube
 
-sphereVolume :: Float -> Float
-sphereVolume radius = (4.0 / 3.0) * pi * (radius ^ 3)
+main = do
+  print (Sphere.area 2)
+  print (Sphere.volume 2)
 
-sphereArea :: Float -> Float
-sphereArea radius = 4 * pi * (radius ^ 2)
+  print (Cuboid.area 2 2 2)
+  print (Cuboid.volume 2 2 2)
 
-cubeVolume :: Float -> Float
-cubeVolume side = cuboidVolume side side side
-
-cubeArea :: Float -> Float
-cubeArea side = cuboidArea side side side
-
-cuboidVolume :: Float -> Float -> Float -> Float
-cuboidVolume a b c = rectangleArea a b * c
-
-cuboidArea :: Float -> Float -> Float -> Float
-cuboidArea a b c = rectangleArea a b * 2 + rectangleArea a c * 2 + rectangleArea c b * 2
-
-rectangleArea :: Float -> Float -> Float
-rectangleArea a b = a * b
+  print (Cube.area 2)
+  print (Cube.volume 2)
